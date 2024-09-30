905db28713b314f518c8f9e625455ac87158029a
CARLOS DÍAZ <diazromerocarlos2@gmail.com>
2024-09-30 20:59:49 +0100

    Primer commit

---
+++ app/src/main/java/com/example/ut1b_carlosdiazromero/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.ut1b_carlosdiazromero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ut1b_carlosdiazromero.ui.theme.UT1B_CarlosDiazRomeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UT1B_CarlosDiazRomeroTheme {
+                conversacion(SampleData.conversationSample)
+            }
+        }
+    }
+}
+
+data class Mensaje(val autor: String, val cuerpo: String)
+
+@Composable
+fun TarjetaDeMensaje(msg: Mensaje){
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.perfil),
+            contentDescription = "Foto de contacto",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){
+            Text(
+                text = msg.autor,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface (
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.cuerpo,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun conversacion(lista: List<Mensaje>){
+    LazyColumn {
+        items(lista) { mensaje ->
+            TarjetaDeMensaje(msg = mensaje)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversacion(){
+    UT1B_CarlosDiazRomeroTheme{
+        conversacion(SampleData.conversationSample)
+    }
+}
